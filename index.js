d20f1257c1931e35e8e14e428328a28bd9b7eed2
Hafidz Abdillah <84985914+Sad404-Cyber@users.noreply.github.com>
2022-06-25 01:57:58 +0700

    update index.js

--- index.js
+++ index.js
@@ -1587,7 +1587,7 @@ break
 							alpha.send5ButGif(from, lang.menunya(salam, pushname, botname) , `© ${ownername}`,pp_bot, [{"urlButton": {"displayText": "Instagram Creator","url": `${instagram}`}},{"quickReplyButton": {"displayText": "Donasi","id": 'donate'}},{"quickReplyButton": {"displayText": "Owner","id": 'owner'}},{"quickReplyButton": {"displayText": "List Command","id": 'command'}}] , {quoted: m})
 						 }
 						if(typemenu == 'katalog'){
-							alpha.sendKatalog(m.chat, "ALL MENU BOTZ", lang.listMenu(time, salam, pushname, prefix), pp_bot, {quoted:m})
+							alpha.sendKatalog(m.chat, "ALL MENU BOT", lang.listMenu(time, salam, pushname, prefix), pp_bot, {quoted:m})
 							}
 						if(typemenu == 'katalog2'){
 							alpha.sendKatalog2(m.chat, "ALL MENU BOT", lang.listMenu(time, salam, pushname, prefix), pp_bot, {quoted:m})

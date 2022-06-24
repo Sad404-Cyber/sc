b64eca767b012cba6149c184b82f4db71a5d16f1
Hafidz Abdillah <84985914+Sad404-Cyber@users.noreply.github.com>
2022-06-25 00:40:03 +0700

    update settings.js

--- settings.js
+++ settings.js
@@ -18,15 +18,15 @@ global.autoread = false // auto read pesan / message
 //global.available = false //status online (online)
 
 // Other
-global.botname = "Alphabot-Mdོ" //namabot kalian
-global.ownername= "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×" //nama kalian
-global.myweb ="https://api.zeeoneofc.xyz" //bebas asal jan hapus
-global.youtube = "https://youtube.com/c/ZeeoneOfc" //bebas asal jan hapus
+global.botname = "Abdillah Botz" //namabot kalian
+global.ownername= "Hafidz Abdillah" //nama kalian
+global.myweb ="https://faxdil-api.herokuapp.com" //bebas asal jan hapus
+global.instagram = "https://instagram/hafidzabdillh_" //bebas asal jan hapus
 global.github = "https://zeeone-ofc.github.io/" //bebas
-global.email = "zeeoneofc@gmail.com" //bebas
-global.region = "Indonesia" //bebas
-global.ownernomer = "62887435047326" // nomor wa kalian
-global.ownernomerr = "+62887435047326" //nmr wa kalian
+global.email = "abdillahhafidz46@gmail.com" //bebas
+global.region = "Semarang, Java" //bebas
+global.ownernomer = "628895656884" // nomor wa kalian
+global.ownernomerr = "+628895656884" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay

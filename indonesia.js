d051d043f1dca02f8d54e20e307f84d6f4e48091
Hafidz Abdillah <84985914+Sad404-Cyber@users.noreply.github.com>
2022-06-25 11:23:49 +0700

    update indonesia.js

--- indonesia.js
+++ indonesia.js
@@ -518,186 +518,39 @@ exports.listMenu = (time, salam, pushname, prefix) => {
 
 
 ╭─❒ 「 Textpro Menu 」
-▸ ${prefix}halloween2 text|text2
-▸ ${prefix}horror text|text2
-▸ ${prefix}game8bit text|text2
-▸ ${prefix}layered text|text2
-▸ ${prefix}glitch2 text|text2
-▸ ${prefix}coolg text|text2
-▸ ${prefix}coolwg text|text2
-▸ ${prefix}realistic text|text2
-▸ ${prefix}space3d text|text2
-▸ ${prefix}gtiktok text|text2
-▸ ${prefix}stone text|text2
-▸ ${prefix}marvel text|text2
-▸ ${prefix}marvel2 text|text2
-▸ ${prefix}pornhub text|text2
-▸ ${prefix}avengers text|text2
-▸ ${prefix}metalr text|text2
-▸ ${prefix}metalg text|text2
-▸ ${prefix}metalg2 text|text2
-▸ ${prefix}halloween2 text|text2
-▸ ${prefix}lion text|text2
-▸ ${prefix}wolf_bw text|text2
-▸ ${prefix}wolf_g text|text2
-▸ ${prefix}ninja text|text2
-▸ ${prefix}3dsteel text|text2
-▸ ${prefix}horror2 text|text2
-▸ ${prefix}lava text|text2
-▸ ${prefix}bagel text|text2
-▸ ${prefix}blackpink text
-▸ ${prefix}rainbow2 text
-▸ ${prefix}water_pipe text
-▸ ${prefix}halloween text
-▸ ${prefix}sketch text
-▸ ${prefix}sircuit text
-▸ ${prefix}discovery text
-▸ ${prefix}metallic2 text
-▸ ${prefix}fiction text
-▸ ${prefix}demon text
-▸ ${prefix}transformer text
-▸ ${prefix}berry text
-▸ ${prefix}thunder text
-▸ ${prefix}magma text
-▸ ${prefix}3dstone text
-▸ ${prefix}neon text
-▸ ${prefix}glitch text
-▸ ${prefix}harry_potter text
-▸ ${prefix}embossed text
-▸ ${prefix}broken text
-▸ ${prefix}papercut text
-▸ ${prefix}gradient text
-▸ ${prefix}glossy text
-▸ ${prefix}watercolor text
-▸ ${prefix}multicolor text
-▸ ${prefix}neon_devil text
-▸ ${prefix}underwater text
-▸ ${prefix}bear text
-▸ ${prefix}wonderfulg text
-▸ ${prefix}christmas text
-▸ ${prefix}neon_light text
-▸ ${prefix}snow text
-▸ ${prefix}cloudsky text
-▸ ${prefix}luxury2 text
-▸ ${prefix}gradient2 text
-▸ ${prefix}summer text
-▸ ${prefix}writing text
-▸ ${prefix}engraved text
-▸ ${prefix}summery text
-▸ ${prefix}3dglue text
-▸ ${prefix}metaldark text
-▸ ${prefix}neonlight text
-▸ ${prefix}oscar text
-▸ ${prefix}minion text
-▸ ${prefix}holographic text
-▸ ${prefix}purple text
-▸ ${prefix}glossyb text
-▸ ${prefix}deluxe2 text
-▸ ${prefix}glossyc text
-▸ ${prefix}fabric text
-▸ ${prefix}neonc text
-▸ ${prefix}newyear text
-▸ ${prefix}newyear2 text
-▸ ${prefix}metals text
-▸ ${prefix}xmas text
-▸ ${prefix}blood text
-▸ ${prefix}darkg text
-▸ ${prefix}joker text
-▸ ${prefix}wicker text
-▸ ${prefix}natural text
-▸ ${prefix}firework text
-▸ ${prefix}skeleton text
-▸ ${prefix}balloon text
-▸ ${prefix}balloon2 text
-▸ ${prefix}balloon3 text
-▸ ${prefix}balloon4 text
-▸ ${prefix}balloon5 text
-▸ ${prefix}balloon6 text
-▸ ${prefix}balloon7 text
-▸ ${prefix}steel text
-▸ ${prefix}gloss text
-▸ ${prefix}denim text
-▸ ${prefix}decorate text
-▸ ${prefix}decorate2 text
-▸ ${prefix}peridot text
-▸ ${prefix}rock text
-▸ ${prefix}glass text
-▸ ${prefix}glass2 text
-▸ ${prefix}glass3 text
-▸ ${prefix}glass4 text
-▸ ${prefix}glass5 text
-▸ ${prefix}glass6 text
-▸ ${prefix}glass7 text
-▸ ${prefix}glass8 text
-▸ ${prefix}captain_as2 text
-▸ ${prefix}robot text
-▸ ${prefix}equalizer text
-▸ ${prefix}toxic text
-▸ ${prefix}sparkling text
-▸ ${prefix}sparkling2 text
-▸ ${prefix}sparkling3 text
-▸ ${prefix}sparkling4 text
-▸ ${prefix}sparkling5 text
-▸ ${prefix}sparkling6 text
-▸ ${prefix}sparkling7 text
-▸ ${prefix}decorative text
-▸ ${prefix}chocolate text
-▸ ${prefix}strawberry text
-▸ ${prefix}koifish text
-▸ ${prefix}bread text
-▸ ${prefix}matrix text
-▸ ${prefix}blood2 text
-▸ ${prefix}neonligth2 text
-▸ ${prefix}thunder2 text
-▸ ${prefix}3dbox text
-▸ ${prefix}neon2 text
-▸ ${prefix}roadw text
-▸ ${prefix}bokeh text
-▸ ${prefix}gneon text
-▸ ${prefix}advanced text
-▸ ${prefix}dropwater text
-▸ ${prefix}wall text
-▸ ${prefix}chrismast text
-▸ ${prefix}honey text
-▸ ${prefix}drug text
-▸ ${prefix}marble text
-▸ ${prefix}marble2 text
-▸ ${prefix}ice text
-▸ ${prefix}juice text
-▸ ${prefix}rusty text
-▸ ${prefix}abstra text
-▸ ${prefix}biscuit text
-▸ ${prefix}wood text
-▸ ${prefix}scifi text
-▸ ${prefix}metalr text
-▸ ${prefix}purpleg text
-▸ ${prefix}shiny text 
-▸ ${prefix}jewelry text
-▸ ${prefix}jewelry2 text
-▸ ${prefix}jewelry3 text
-▸ ${prefix}jewelry4 text
-▸ ${prefix}jewelry5 text
-▸ ${prefix}jewelry6 text
-▸ ${prefix}jewelry7 text
-▸ ${prefix}jewelry8 text
-▸ ${prefix}metalh text
-▸ ${prefix}golden text
-▸ ${prefix}glitter text
-▸ ${prefix}glitter2 text
-▸ ${prefix}glitter3 text
-▸ ${prefix}glitter4 text
-▸ ${prefix}glitter5 text
-▸ ${prefix}glitter6 text
-▸ ${prefix}glitter7 text
-▸ ${prefix}metale text
-▸ ${prefix}carbon text
-▸ ${prefix}candy text
-▸ ${prefix}metalb text
-▸ ${prefix}gemb text
-▸ ${prefix}3dchrome text
-▸ ${prefix}metalb2 text
-▸ ${prefix}metalg text
-╰❒ ${prefix}metalg text
+▸ #blackpink
+▸ #neon
+▸ #greenneon
+▸ #advanceglow
+▸ #futureneon
+▸ #sandwriting
+▸ #sandsummer
+▸ #sandengraved
+▸ #metaldark
+▸ #neonlight
+▸ #holographic
+▸ #text1917
+▸ #minion
+▸ #deluxesilver
+▸ #newyearcard
+▸ #bloodfrosted
+▸ #halloween
+▸ #jokerlogo
+▸ #fireworksparkle
+▸ #natureleaves
+▸ #bokeh
+▸ #toxic
+▸ #strawberry
+▸ #box3d
+▸ #roadwarning
+▸ #breakwall
+▸ #icecold
+▸ #luxury
+▸ #cloud
+▸ #summersand
+▸ #horrorblood
+▸ #thunder
+╰❒ 
 
 
 ╭─❒ 「 Others 」
